a881647c942850dd84230399021e7a3b2365f1f1
Mohammad-Amirifard <m.amirifard.94@gmail.com>
2024-09-08 00:04:46 +0200

    Added some details to setup.py file

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 import setuptools
 
+'''
+This file sets up the whole package with some detail.
+Using this setup file and requirements.txt including -e . at the ened, you can install required packages
+in a editable mode.
+'''
 Aouthor= "Mohammad Amirifard"
 Email = "Mohammad.amirifard@mail.polimi.it"
-Project_name = "Kideney_Disease Classification"
+Src_repo = "Kideney_classifier" # When we set this to name of folder in src, we can add logger in __ini__ file in src folder
 Vesrion = '0.0.1'
 
 setuptools.setup(
-    name=Project_name,
+    name=Src_repo,
     version=Vesrion,
     author= Aouthor,
     packages=setuptools.find_packages(where="src")
